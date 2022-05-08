13e25b386503a9c702e849769fdbd47e80de6f65
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-05-08 19:47:33 +0200

    Print funktion

---
+++ Python guide/print.py
@@ -0,0 +1,34 @@
+# Det første man skal lære i alle programmerings sprog 
+# Er hvordan man får computeren til at skrive en ting ud i din terminal.
+
+# Det kan man nemt gøre ved hjælp af print funktion.
+# Print funktion består af print også ()
+# "" er ikke nødventig det gæler om det der skal stå inde i. Det kommer vi til lige efter denne her. 
+print("Hej Verden!")
+
+
+# Print funktion kan også vise andet end bare text den kan også printe tal og variabel.
+# Hvis vi starter bare med at skulle vise tal så skal man bare.
+print(1)
+
+# Den hviser bare 1. Man kan også få den til at plusse og minus og aller andre ting som.
+print(1+2)
+# Den printer 3 
+
+# Men som sagt så kan den også bruge variabel til at printe.
+# Så hvis vi laver en variabel så skal man i python skrive et navn = Det den skal gemme 
+x = 1 
+# det er en variabel som hedder x og den gemmer 1 i den. Nu laver vi lidt flere også sætter vi den ind i en print funktion.
+
+x = 1 
+y = 2 
+print(x + y)
+# Den printer 3. Som vi kan se skal der ikke bruges "" der skal bare så variabelns navn også fordi vi vil pulse så er der et + tegn som der siger de skal plusses. 
+# Man kan også få den til at minus ved at skrive -
+
+print(x - y)
+# Den printer -1 nu. som sagt så kan man også gange og dividere osv. 
+
+# Man kan også sætte variabel og text inde i en print 
+# Man skal bare huske , når vi skifter imellem typen af det der står inde i print funktion.
+print("Minst", x, "person sover lige nu")
