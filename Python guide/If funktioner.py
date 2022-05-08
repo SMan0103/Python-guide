a177bd537f924d22896d3b7af563a77f769685f3
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-05-08 21:29:10 +0200

    If Funktion opdateret

--- Python guide/If funktioner.py
+++ Python guide/If funktioner.py
@@ -5,4 +5,29 @@ x = 1
 y = 1 
 if x == y :
     print("X er lige så stor som y")
+# Her vil den printe det der står i print funktionen. 
+# Det giver også god mening når vi siger at x == y hvor begge har samme værdi gemt i sig. 
+
+# Til at vise en funkion som der ikke giver mening vil det se sådan her ud.
+
+# x = 2 
+# y = 1 
+# if x == y : 
+#   print("X er lige så stor som y")
+
+
+# den viser fejl fordi at det ikke er det samme 
+# Der kan vi nemt fixe det ved at lave en Else funktion som er en funktion som høre inde under if funktion 
+# Her vil jeg vise hvordan se vil se ud 
+
+x = 2
+y = 1
+if x == y :
+    print("X er lige så stor som y")
+else:
+    print("X og y er ikke det samme")
+# Her vil den printe det der stor i else fordi at x og y er ikke det samme. 
+
+# Så i en if funktion aklare du noget først til at sige hvad den skal gøre hvis det er opfyldt.
+# Hvis det ikke bliver opfyldt så vil den gå til else funktionen.
 
