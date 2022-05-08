ce85e5a1b4f58c5279a9a95efe1c0d18e9f65de5
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-05-08 21:06:59 +0200

    If Funktionen

---
+++ Python guide/If funktioner.py
@@ -0,0 +1,8 @@
+# If funktioner er noget af det vigtigste at kunne forstå når det kommer til programering.
+# If funktioner er en funktion som der fortæller hvis en ting er på en måde så skal der ske noget.
+# Det kan ses på den funktion nede under.
+x = 1 
+y = 1 
+if x == y :
+    print("X er lige så stor som y")
+
